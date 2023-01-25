fbff962394c7277d71d559b4276affbbd4830c3e
joango95 <joango95@gmail.com>
2023-01-24 23:42:40 -0500

    Adds error handling

--- src/main/java/com/joango/exception/TicketNotFoundException.java
+++ src/main/java/com/joango/exception/TicketNotFoundException.java
@@ -1,5 +1,9 @@
 package com.joango.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class TicketNotFoundException extends RuntimeException{
 
     public TicketNotFoundException() {
